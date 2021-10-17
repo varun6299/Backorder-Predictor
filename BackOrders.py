805f4584427b5354d1cbf2e15750f806405a00a4
Varun Sagar Theegala <VarunSagar06Official@gmail.com>
2021-10-17 10:14:48 +0530

    Update BackOrders.py

--- BackOrders.py
+++ BackOrders.py
@@ -68,13 +68,13 @@ def Results():
 
         else:
             result = "A Backorder"
-        with open("Logging\Logging.txt","a") as logger:
+        with open("Logging/Logging.txt","a") as logger:
             dtobj = datetime.datetime.now(tz=gettz('Asia/Kolkata'))
             date = str(datetime.datetime.fromtimestamp(round(dtobj.timestamp())))
             logger.write(f"{date} INFO-Prediction successful. Prediction made: {result}\n")
             
     except BaseException as logger:
-        with open("Logging\Logging.txt","a") as logger_file:
+        with open("Logging/Logging.txt","a") as logger_file:
             dtobj = datetime.datetime.now(tz=gettz('Asia/Kolkata'))
             date = str(datetime.datetime.fromtimestamp(round(dtobj.timestamp())))
             logger_file.write(f"{date} ERROR {logger}-Error has occured")
